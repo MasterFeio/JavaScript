ab409f48cba25d03f5767ddee10b2176f31d07c7
MasterFeio <96741685+MasterFeio@users.noreply.github.com>
2023-05-05 13:41:14 -0300

    Condições

---
+++ Aula11/ex01.js
@@ -0,0 +1,10 @@
+var vel = 78;
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+
+if (vel > 60){ //Condição Simples
+    console.log('Você passou a velocidade permitida. MULTADO!');
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`);
+
